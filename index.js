5431fc0df9a25f8a129d0435d3658f7139a28661
Kirk Santos <kirk.santos@carbonetes.com>
2023-03-13 11:25:07 +0800

    add buildFlagArgs

--- index.js
+++ index.js
@@ -136,12 +136,44 @@ function checkSecretsContentRegex() {
 }
 
 // Upload SBOM as an artifact
-async function uploadSBOM(sbomFile) {
+async function uploadSBOM() {
+    let sbomFile = core.getInput('output_file')
     if (sbomFile === null || sbomFile === '') return
     const client = artifact.create()
     const files = [sbomFile]
     const rootDir = "."
     await client.uploadArtifact(sbomFile, files, rootDir)
+}
+
+// Build flag args
+function buildFlagArgs(args) {
+    // Check for disable file listing
+    if (checkDisableFileListing()) args.push('--disable-file-listing')
+
+    // Check for disable secret search
+    if (checkDisableSecretSearch()) args.push('--disable-secret-search')
+
+    // Check for output file
+    const outputFile = checkOutputFile()
+    if (outputFile !== null) args.push('-f', outputFile)
+
+    // Check for enabled parsers
+    const enabledParsers = checkEnabledParsers()
+    if (enabledParsers !== ALL) args.push(`--enabled-parsers=${enabledParsers}`)
+
+    // Check for secret exclude filenames
+    const secretExcludeFilenames = checkSecretExcludeFilenames()
+    if (secretExcludeFilenames !== null) args.push(`--secret-exclude-filenames=${secretExcludeFilenames}`)
+
+    // Check for secret max file size
+    const secretMaxFileSize = checkSecretMaxFileSize()
+    if (secretMaxFileSize !== null) args.push(`--secret-max-file-size=${secretMaxFileSize}`)
+
+    // Check for secrets content regex
+    const secretsContentRegex = checkSecretsContentRegex()
+    if (secretsContentRegex !== null) args.push(`--secrets-content-regex=${secretsContentRegex}`)
+
+    return args
 }
 
 async function constructCommandExec(scanOption) {
@@ -150,39 +182,13 @@ async function constructCommandExec(scanOption) {
         case DIRECTORY:
             // Check for output type
             const outputType = checkOutputType()
-            let args = ["-d", directoryInput, "-o", outputType]
-
-            // Check for enabled parsers
-            const enabledParsers = checkEnabledParsers()
-            if (enabledParsers !== ALL) args.push(`--enabled-parsers=${enabledParsers}`)
-
-            // Check for output file
-            const outputFile = checkOutputFile()
-            if (outputFile !== null) args.push('-f', outputFile)
-
-            // Check for disable file listing
-            if (checkDisableFileListing()) args.push('--disable-file-listing')
-
-            // Check for disable secret search
-            if (checkDisableSecretSearch()) args.push('--disable-secret-search')
-
-            // Check for secret exclude filenames
-            const secretExcludeFilenames = checkSecretExcludeFilenames()
-            if (secretExcludeFilenames !== null) args.push(`--secret-exclude-filenames=${secretExcludeFilenames}`)
-
-            // Check for secret max file size
-            const secretMaxFileSize = checkSecretMaxFileSize()
-            if (secretMaxFileSize !== null) args.push(`--secret-max-file-size=${secretMaxFileSize}`)
-
-            // Check for secrets content regex
-            const secretsContentRegex = checkSecretsContentRegex()
-            if (secretsContentRegex !== null) args.push(`--secrets-content-regex=${secretsContentRegex}`)
+            const args = buildFlagArgs(["-d", directoryInput, "-o", outputType])
 
             // Execute Diggity
             exec.exec('./bin/diggity', args)
             .then(()=>{
                 // Upload SBOM
-                uploadSBOM(outputFile)
+                uploadSBOM()
             })
 
             break;
